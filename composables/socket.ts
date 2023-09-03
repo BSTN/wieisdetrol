a0cb77e7b82d63fb75284396f3a9e91ec662ced9
BSTN <basten@bstn.nl>
2023-09-03 14:40:40 +0100

    update typescripts etc

--- composables/socket.ts
+++
@@ -1,7 +0,0 @@
-import { io, Socket } from "socket.io-client";
-const config = useRuntimeConfig()
-const started: Socket<ServerToClientEvents, ClientToServerEvents> = io(config.public.URL + config.public.BASE)
-
-export const useSocket = () => {
-  return started
-}
