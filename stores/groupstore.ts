63cec8193f0f50b1c696b64956fa6868d6cf4472
BSTN <basten@bstn.nl>
2024-08-28 12:23:08 +0200

    back to polling

--- stores/groupstore.ts
+++ stores/groupstore.ts
@@ -62,7 +62,7 @@ export const useGroupStore = defineStore('groupStore', {
       // }
 
       const config = useRuntimeConfig()
-      SOCK = io(config.public.URL + config.public.BASE, {transports: ['websocket']})
+      SOCK = io(config.public.URL + config.public.BASE, {transports: ['polling']})
 
       // connection status
       SOCK.on('connect', function () { 
