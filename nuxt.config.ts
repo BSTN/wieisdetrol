692bd49f2411530db0b677a5d60e3f034a2f18a1
BSTN <basten@bstn.nl>
2025-01-16 15:18:15 +0000

    monitor env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,9 @@ import ViteYaml from '@modyfi/vite-plugin-yaml';
 import ViteMarkdown from 'vite-plugin-markdown';
 import svgLoader from 'vite-svg-loader'
 
+console.log(process.env.BASE)
+console.log(process.env.URL)
+
 export default defineNuxtConfig({
   css: [
     '@/less/elements.less'
